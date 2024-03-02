1d83e0249ba8cf68dca75ce2883af92821ebc2dc
GunPKate <gunPK>
2024-03-02 11:04:31 +0700

    pages by router dom

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { BrowserRouter,RouterProvider,createBrowserRouter } from 'react-router-dom'
+import FormInput from './FormInput.jsx'
+
+const router = createBrowserRouter([
+
+  {
+    path: "/",
+    element: <FormInput/>
+  },
+  {
+    path: "/home",
+    element: <h2>Home</h2>
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
